bdabb5a56e6183391a239c45ec521f1252ccde2c
HugoNCastro <hugonaaman@gmail.com>
2023-07-04 11:25:06 -0300

    Adiciona regex para capturar o valor do id

--- src/server.js
+++ src/server.js
@@ -3,15 +3,21 @@ import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
 
-const server = http.createServer( async (req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  const route = routes.find(route => route.method === method && route.path === url)
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
   if (route) {
-   return route.handler(req, res)
+    const routeParams = req.url.match(route.path)
+
+    console.log(routeParams)
+
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end();
